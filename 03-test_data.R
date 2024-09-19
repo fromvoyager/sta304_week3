f9ad96042066199e4ca2d5ce41cf3ccf0adf2305
fromvoyager <180869692+fromvoyager@users.noreply.github.com>
2024-09-19 17:54:16 -0400

    Add files via upload

---
+++ 03-test_data.R
@@ -0,0 +1,18 @@
+#### Preamble ####
+# Purpose: Tests to verify the simulated data is what we wanted.
+# Author: Melissa Xiao
+# Date: 19 September 2024
+# Contact: melissa.xiao@mail.utoronto.ca
+# License: University of Toronto
+
+#### Workspace setup ####
+library(tidyverse)
+
+#### Test data ####
+data <- read_csv("data/raw_data/simulated.csv")
+
+# Test for negative numbers
+data$number_of_marriage |> min() <= 0
+
+# Test for NAs
+all(is.na(data$number_of_marriage))
